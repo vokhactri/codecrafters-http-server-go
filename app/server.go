24d097512b051b693bb40424a8d13c9efbdb9fec
Tri Vo Khac <trivk@vng.com.vn>
2024-03-28 10:51:55 +0700

    fix 6th stage

--- app/server.go
+++ app/server.go
@@ -8,19 +8,23 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	// Accept and handle incoming connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
 
-	go handleConnection(conn)
+		// Handle connection concurrently
+		go handleClient(conn)
+	}
 }
 
 func formatResponseContent(content string) string {
@@ -35,32 +39,35 @@ func formatResponseContent(content string) string {
 		"\r\n")
 }
 
-func handleConnection(conn net.Conn) {
+func handleClient(conn net.Conn) {
 	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		os.Exit(1)
-	}
 
-	bufferArr := strings.Split(string(buffer[:n]), "\r\n")
-	startLine := bufferArr[0]
-	path := strings.Split(startLine, " ")[1]
-	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		return
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		bufferArr := strings.Split(string(buffer[:n]), "\r\n")
+		startLine := bufferArr[0]
+		path := strings.Split(startLine, " ")[1]
+		if path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			return
+		}
+		if strings.HasPrefix(path, "/echo/") {
+			randomString := strings.TrimPrefix(path, "/echo/")
+			content := formatResponseContent(randomString)
+			conn.Write([]byte(content))
+			return
+		}
+		if path == "/user-agent" {
+			user_agent := strings.Split(bufferArr[2], " ")[1]
+			content := formatResponseContent(user_agent)
+			conn.Write([]byte(content))
+			return
+		}
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	if strings.HasPrefix(path, "/echo/") {
-		randomString := strings.TrimPrefix(path, "/echo/")
-		content := formatResponseContent(randomString)
-		conn.Write([]byte(content))
-		return
-	}
-	if path == "/user-agent" {
-		user_agent := strings.Split(bufferArr[2], " ")[1]
-		content := formatResponseContent(user_agent)
-		conn.Write([]byte(content))
-		return
-	}
-	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
