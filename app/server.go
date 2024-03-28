aa36504d4ce7036f60bc25e9adfb620d38b53c4d
Tri Vo Khac <trivk@vng.com.vn>
2024-03-28 10:11:49 +0700

    pass 4th stage

--- app/server.go
+++ app/server.go
@@ -29,11 +29,18 @@ func main() {
 
 	start_line := strings.Split(string(buffer[:n]), "\r\n")[0]
 	path := strings.Split(start_line, " ")[1]
-
-	switch path {
-	case "/":
+	if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	default:
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
 	}
+	if strings.HasPrefix(path, "/echo/") {
+		random_string := strings.TrimPrefix(path, "/echo/")
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
+		conn.Write([]byte("Content-Type: text/plain\r\n"))
+		conn.Write([]byte(fmt.Sprintf("Content-Length: %d\r\n", len(random_string))))
+		conn.Write([]byte("\r\n"))
+		conn.Write([]byte(fmt.Sprintf("%s\r\n\r\n", random_string)))
+		return
+	}
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
