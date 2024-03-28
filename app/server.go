b6ff686b1339535b776322eb482210e906ef9064
Tri Vo Khac <trivk@vng.com.vn>
2024-03-28 09:51:46 +0700

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -2,28 +2,38 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	conn.Write(([]byte)("HTTP/1.1 200 OK\r\n\r\n"))
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	start_line := strings.Split(string(buffer[:n]), "\r\n")[0]
+	path := strings.Split(start_line, " ")[1]
+
+	switch path {
+	case "/":
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
+	default:
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n"))
+	}
 }
